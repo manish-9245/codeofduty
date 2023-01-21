8e0a575d8597483e6eb0e20ef6be408f7d4775c1
dhruvja <dhruv@iamsizzling.com>
2023-01-21 15:30:12 +0530

    added dasbhoard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,8 @@
-import StatsCard from "../components/filelist";
+
 import Fileupload from "../components/fileupload";
 import WithSubnavigation from "../components/navbar";
 import * as React from "react";
+import BasicStatistics from "../components/filelist"
 import {
   ChakraProvider,
   Box,
@@ -16,7 +17,7 @@ import {
 export const dash = () => (
   <>
     <WithSubnavigation />
-    <StatsCard />
-    <Fileupload />
+    <BasicStatistics />
   </>
+    
 );
